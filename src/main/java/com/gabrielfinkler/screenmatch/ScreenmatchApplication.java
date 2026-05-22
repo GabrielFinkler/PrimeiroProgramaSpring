8547608a123bf31602ad85cf6e6e210707e84be8
GabrielCremonese <gabrielfcremonese@gmail.com>
2026-05-21 22:05:27 -0300

    modelando temporadas

--- src/main/java/com/gabrielfinkler/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/gabrielfinkler/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package com.gabrielfinkler.screenmatch;
 
 import com.gabrielfinkler.screenmatch.model.DadosEpisodio;
 import com.gabrielfinkler.screenmatch.model.DadosSerie;
+import com.gabrielfinkler.screenmatch.model.DadosTemporada;
 import com.gabrielfinkler.screenmatch.service.ConsumoApi;
 import com.gabrielfinkler.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -28,5 +32,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i <= dados.totalTemporadas();i++ ){
+			json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season="+i+"&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
---
+++ src/main/java/com/gabrielfinkler/screenmatch/model/DadosTemporada.java
@@ -0,0 +1,11 @@
+package com.gabrielfinkler.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") int numero,
+                             @JsonAlias("Episodes")List<DadosEpisodio> episodios) {
+}
